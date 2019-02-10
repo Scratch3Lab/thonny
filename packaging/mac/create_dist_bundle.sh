967e6844f4228277ae9c924af81d865e914bb273
wwj718 <wuwenjie718@gmail.com>
2019-02-10 17:21:44 +0800

    build jupyterlab

--- packaging/mac/create_dist_bundle.sh
+++ packaging/mac/create_dist_bundle.sh
@@ -23,19 +23,19 @@ FRAMEWORKS=build/Thonny.app/Contents/Frameworks
 PYTHON_CURRENT=$FRAMEWORKS/Python.framework/Versions/3.7/
 
 # install deps #####################################################
-$PYTHON_CURRENT/bin/python3.7 -m pip install jedi==0.13.2
-$PYTHON_CURRENT/bin/python3.7 -m pip install mypy==0.660
-$PYTHON_CURRENT/bin/python3.7 -m pip install pylint==2.2.2
-$PYTHON_CURRENT/bin/python3.7 -m pip install docutils==0.14
-$PYTHON_CURRENT/bin/python3.7 -m pip install pyserial==3.4
-$PYTHON_CURRENT/bin/python3.7 -m pip install pyperclip==1.7.0
+# $PYTHON_CURRENT/bin/python3.7 -m pip install jedi==0.13.2
+# $PYTHON_CURRENT/bin/python3.7 -m pip install mypy==0.660
+# $PYTHON_CURRENT/bin/python3.7 -m pip install pylint==2.2.2
+# $PYTHON_CURRENT/bin/python3.7 -m pip install docutils==0.14
+# $PYTHON_CURRENT/bin/python3.7 -m pip install pyserial==3.4
+# $PYTHON_CURRENT/bin/python3.7 -m pip install pyperclip==1.7.0
 
 # install certifi #####################################################
 $PYTHON_CURRENT/bin/python3.7 -m pip install certifi
 
 # install thonny #####################################################
-$PYTHON_CURRENT/bin/python3.7 -m pip install --pre --no-cache-dir thonny
-rm $PYTHON_CURRENT/bin/thonny # because this contains absolute paths
+$PYTHON_CURRENT/bin/python3.7 -m pip install --pre --no-cache-dir jupyterlab
+# rm $PYTHON_CURRENT/bin/jupyterlab # because this contains absolute paths
 
 # clean unnecessary stuff ###################################################
 
@@ -56,7 +56,7 @@ find $PYTHON_CURRENT/lib -name '*.pyc' -delete
 find $PYTHON_CURRENT/lib -name '*.exe' -delete
 rm -rf $PYTHON_CURRENT/Resources/English.lproj/Documentation
 
-rm -rf $PYTHON_CURRENT/share
+# rm -rf $PYTHON_CURRENT/share
 rm -rf $PYTHON_CURRENT/lib/python3.7/test
 rm -rf $PYTHON_CURRENT/lib/python3.7/idlelib
 
@@ -82,7 +82,7 @@ cp thonny_python.ini $PYTHON_CURRENT/bin
 cp -f $SCRIPT_DIR/Python.app.plist $PYTHON_CURRENT/Resources/Python.app/Contents/Info.plist
 
 # version info ##############################################################
-VERSION=$(<$PYTHON_CURRENT/lib/python3.7/site-packages/thonny/VERSION)
+VERSION="v0.1" #$(<$PYTHON_CURRENT/lib/python3.7/site-packages/thonny/VERSION)
 ARCHITECTURE="$(uname -m)"
 VERSION_NAME=thonny-$VERSION-$ARCHITECTURE 
 
@@ -99,14 +99,14 @@ rm -f build/Thonny.app/Contents/Info.plist.bak
 cp readme.txt build
 
 # create dmg #####################################################################
-mkdir -p dist
-FILENAME=dist/thonny-${VERSION}.dmg
-rm -f $FILENAME
-hdiutil create -srcfolder build -volname "Thonny $VERSION" $FILENAME
-hdiutil internet-enable -yes $FILENAME
-
+# mkdir -p dist
+# FILENAME=dist/thonny-${VERSION}.dmg
+# rm -f $FILENAME
+# hdiutil create -srcfolder build -volname "Thonny $VERSION" $FILENAME
+# hdiutil internet-enable -yes $FILENAME
+# Q: share目录不存在
 # sign dmg ######################
 # codesign -s "Aivar Annamaa" --keychain ~/Library/Keychains/login.keychain $FILENAME
 
 # clean up
-rm -rf build
+# rm -rf build
